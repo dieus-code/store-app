b23f8a9ebc221fa6ad9af6c7089aa792dac9ba1b
dieus-code <dieusamanda21@gmail.com>
2026-07-13 11:26:32 +0000

    page fix

--- app/components/products.tsx
+++ app/components/products.tsx
@@ -1,11 +1,12 @@
 import react from "react";
+import { Link } from "react-router";
 import hat from "../assets/images/hat.jpg";
 
 export default function Products() {
 
   return (
     <div >
-      <a href="/product">
+      <Link to="/product">
      <div className="bg-blue-400 border border-gray-800 w-70 h-100 p-4 rounded-lg shadow-md hover:shadow-blue-50 transition duration-300 " >
          <div >
           <img src={hat} alt="Product Image" className="w-70 h-75" />
@@ -14,7 +15,7 @@ export default function Products() {
         <p>$19.99</p>
        
       </div>
-      </a>
+      </Link>
     </div>
   );
 }
--- app/routes/product-page.tsx
+++ app/routes/product-page.tsx
@@ -12,24 +12,19 @@ export default function ProductPage() {
   return (
     <div className="bg-blue-400  h-screen">
         
-     <div className="bg-white border border-gray-800 w-70 h-100 p-4 rounded-lg shadow-md hover:shadow-blue-50 transition duration-300" onClick={() => alert("Product clicked!")}>
-         <div >
-          <img src={hat} alt="Product Image" className="w-70 h-75" />
-        </div>
-        </div>
+     <div className=" flex bg-white border border-gray-800  h-100 p-4 rounded-lg shadow-md hover:shadow-blue-50 transition duration-300" onClick={() => alert("Product clicked!")}>
+         
+           <img src={hat} alt="Product Image" className="w-70 h-75 m-10"/>
+           <img src={hat2} alt="Model Image 2" className="w-70 h-75 m-10 object-cover rounded" />
+            <img src={hat3} alt="Model Image 3" className="w-70 h-75 m-10 object-cover rounded" />
+            <img src={hat4} alt="Model Image 4" className="w-70 h-75 m-10 object-cover rounded" />
+       
+      </div>
         <p>name: baseball hats</p>
         <p>price: $19.99</p>
         <p>Description of the product.</p>
 
-       <div className="bg-white p-4 rounded-lg shadow-md">
-        <h2 className="text-lg font-semibold mb-2">Model Images</h2>
-        <div className="flex flex-wrap gap-4">
-          <img src={hat1} alt="Model Image 1" className="w-32 h-32 object-cover rounded" />
-          <img src={hat2} alt="Model Image 2" className="w-32 h-32 object-cover rounded" />
-          <img src={hat3} alt="Model Image 3" className="w-32 h-32 object-cover rounded" />
-          <img src={hat4} alt="Model Image 4" className="w-32 h-32 object-cover rounded" />
-        </div>
-      </div>
+      
     </div>
   );
 }
